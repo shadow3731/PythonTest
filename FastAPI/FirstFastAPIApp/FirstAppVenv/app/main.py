591a45477908d5eba44db7621550fe5faeedbc6f
Vladislav <v1rus3731@gmail.com>
2024-05-29 18:35:53 +0300

    finish exploring the base

--- FastAPI/FirstFastAPIApp/FirstAppVenv/app/main.py
+++ FastAPI/FirstFastAPIApp/FirstAppVenv/app/main.py
@@ -1,23 +1,38 @@
-from fastapi import Depends, FastAPI
+from typing import Annotated
 
-from .dpendencies import get_query_token, get_token_header
-from .internal import admin
-from .routers import items, users
+from fastapi import FastAPI, Header, HTTPException
+from pydantic import BaseModel
 
-app = FastAPI(dependencies=[Depends(get_query_token)])
+fake_secret_token = "coneofsilence"
 
+fake_db = {
+    "foo": {"id": "foo", "title": "Foo", "description": "There goes my hero"},
+    "bar": {"id": "bar", "title": "Bar", "description": "The bartenders"},
+}
 
-app.include_router(users.router)
-app.include_router(items.router)
-app.include_router(
-    admin.router,
-    prefix="/admin",
-    tags=["admin"],
-    dependencies=[Depends(get_token_header)],
-    responses={418: {"description": "I'm a teapot"}},
-)
+app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello Bigger Applications!"}
+class Item(BaseModel):
+    id: str
+    title: str
+    description: str | None = None
+
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_main(item_id: str, x_token: Annotated[str, Header()]):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return fake_db[item_id]
+
+
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item, x_token: Annotated[str, Header()]):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail="Invalid X-Token header")
+    if item.id in fake_db:
+        raise HTTPException(status_code=400, detail="Item already exists")
+    fake_db[item.id] = item
+    return item
