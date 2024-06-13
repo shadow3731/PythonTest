6a33e8da6035df719c1081d141c14aa5b97002db
Vladislav <v1rus3731@gmail.com>
2024-06-13 17:40:34 +0300

    solved leet code task

--- LeetCode/88_merge_sorted_array.py
+++ LeetCode/88_merge_sorted_array.py
@@ -1,24 +1,60 @@
+# Solved
+
+# By 13.06.2024:
+# Runtime = 44 ms (beats 25.35% of users)
+# Memory = 16.59 MB (beats 64.11% of users)
 class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        # Check if 1st list is empty or "m" value equals to 0
         if nums1 is None or m == 0:
-            nums2[:n]
+            # Clear the 1st list
+            nums1.clear()
+            # Insert "n" values from the 2nd list to the 1st one
+            nums1.extend(nums2[:n])
         
+        # Check if 2nd list is empty or "n" value equals to 0
         elif nums2 is None or n == 0:
-            nums1[:m]
-        
-        nums1 = nums1[:m]
-        nums2 = nums2[:n]
-        i, j = 0, 0
-            
-        while i < len(nums1) and j < n:
-            if nums1[i] > nums2[j]:
-                nums1.insert(i, nums2[j])
-                j += 1    
+            # While the length of the 1st list is greater than "m"
+            while len(nums1) > m:
+                # Delete last values from the 1st list
+                nums1.pop()
                 
-            i += 1
+        else:
+            # While the length of the 1st list is greater than "m"
+            while len(nums1) > m:
+                # Delete last values from the 1st list
+                nums1.pop()
             
-        if j < len(nums2):
-            nums1.extend(nums2[j:])
+            # While the length of the 2nd list is greater than "n"    
+            while len(nums2) > n:
+                # Delete last values from the 2nd list
+                nums2.pop()
+            
+            # Variables to point to the indexes of 2 lists for 
+            # comparison    
+            i, j = 0, 0
+            
+            # While the 1st pointer doesn't go out of the 1st 
+            # list length and the 2nd pointer is less than "n"    
+            while i < len(nums1) and j < n:
+                # Comparison between certain values of 2 lists. 
+                # Checking if a certain value from the 1st list 
+                # is greater than from the 2nd one
+                if nums1[i] > nums2[j]:
+                    # Insert "j" value of the 2nd list into the 
+                    # "i" position of the 1st list
+                    nums1.insert(i, nums2[j])
+                    # Move the 2nd pointer next
+                    j += 1    
+                
+                # Move the 1st pointer next    
+                i += 1
+            
+            # Check if there's still left not inserted values 
+            # from the 2nd list    
+            if j < len(nums2):
+                # Insert left values to the end of the 1st list
+                nums1.extend(nums2[j:])
             
 if __name__ == '__main__':
     nums1 = [1, 2, 3, 0, 0, 0]
