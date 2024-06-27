178460de00809325526965bb85e96ac3ecdaded3
Vladislav <v1rus3731@gmail.com>
2024-06-27 10:37:12 +0300

    solved leet code problem

---
+++ LeetCode/136_single_number.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # Set initial value
+        result = 0
+        
+        # A cycle to go through every value of the list
+        for num in nums:
+            # Make xor operation on every value. So that only 
+            # non repeating value will stay
+            result ^= num
+        
+        # Return a unique value
+        return result
+        
+if __name__ == '__main__':
+    nums = [2, 2, 1]
+    print(Solution().singleNumber(nums))
+    
+    nums = [4, 1, 2, 1, 2]
+    print(Solution().singleNumber(nums))
+    
+    nums = [1]
+    print(Solution().singleNumber(nums))
